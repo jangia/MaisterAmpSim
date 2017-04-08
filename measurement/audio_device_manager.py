b5bf5a1fae5311baaa4bf6b684106bf426a76b3b
giacomellij <jan.giacomellij@gmail.com>
2017-04-08 13:22:30 +0200

    Class with record and play functions

---
+++ measurement/audio_device_manager.py
@@ -0,0 +1,47 @@
+import scipy.io.wavfile
+import sounddevice as sd
+
+
+class AudioDeviceManager:
+
+    def __init__(self, filepath, fs=44100, duration=10, channels=2, dtype='float64'):
+        self.filepath = filepath
+        self.fs = fs
+        self.duration = duration
+        self.channels = channels
+        self.dtype = dtype
+
+    def record(self):
+        """
+        Function for recording on your recording device
+        writes your recording to file
+        :return: void
+        """
+
+        try:
+            print(sd.query_devices(sd.default.device)['name'])
+
+            print('Starting recording')
+
+            myrecording = sd.rec(
+                int(self.duration * self.fs),
+                samplerate=self.fs,
+                channels=self.channels,
+                dtype=self.dtype)
+            print('Recording ...')
+            sd.wait()
+
+            print('Writing to file: {0}'.format(self.filepath))
+            scipy.io.wavfile.write(self.filepath, self.fs, myrecording)
+
+        except Exception as e:
+            print("Error: unable to record".format(e))
+
+    def play(self, samples):
+
+        try:
+            sd.play(samples)
+
+            sd.wait()
+        except Exception as e:
+            print('Error trying to play: {0}'.format(e))
