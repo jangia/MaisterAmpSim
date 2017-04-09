002c9ee02e1ef31b2fcede8d80c9d5f7deb7dc95
giacomellij <jan.giacomellij@gmail.com>
2017-04-09 15:42:40 +0200

    Thread for recording an playing sound

--- measurement/generators.py
+++ measurement/generators.py
@@ -2,28 +2,30 @@ import numpy as np
 from scipy import signal
 
 
-def generate_sine_wave(f, volume=0.5, fs=44100, duration=5.0):
-    """
-    :param f: integer - frequency that you want to generate
-    :param volume: float - between [0, 1]
-    :param fs: integer - sampling frequency
-    :return: samples: numpy array of sine wave
-    """
-    samples = volume*(np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+class Generators:
 
-    return samples
+    def __init__(self, f0, f1, t_end, fs, volume):
 
+        self.f0 = f0
+        self.f1 = f1
+        self.t_end = t_end
+        self.fs = fs
+        self.volume = volume
 
-def generate_square_wave(f, volume=0.5, fs=44100, duration=5.0):
-    """
-    :param f: integer - frequency that you want to generate
-    :param volume: float - between [0, 1]
-    :param fs: integer - sampling frequency
-    :return: samples: numpy array of sine wave
-    """
+    def generate_sine_wave(self, f, duration, volume=0.5, fs=44100):
+        """
+        :param f: integer - frequency that you want to generate
+        :param volume: float - between [0, 1]
+        :param fs: integer - sampling frequency
+        :return: samples: numpy array of sine wave
+        """
+        samples = volume*(np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float64)
 
-    t = np.linspace(0, 1, 500, endpoint=False)
-    samples = signal.square(2 * np.pi * 5 * t)
-    # samples = volume*(np.square(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+        return samples
 
-    return samples
+    def generate_sweep(self):
+
+        t = np.linspace(0, self.t_end, self.fs*self.t_end)
+        samples = self.volume*signal.chirp(t, f0=self.f0, f1=self.f1, t1=self.t_end).astype(np.float64)
+
+        return samples
--- measurement/threads.py
+++ measurement/threads.py
@@ -1,19 +1,32 @@
 import threading
 
+import scipy.io.wavfile
+import sounddevice as sd
+import time
+
+from measurement.generators import Generators
+
 
 class AudioInterfaceThread (threading.Thread):
 
-    def __init__(self, thread_id, name, audio_device_manager):
+    def __init__(self, thread_id, name, audio_device_manager, generator):
         threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.name = name
         self.audio_device_manager = audio_device_manager
+        self.generator = generator
 
     def run(self):
         print("Starting " + self.name)
         if self.name == 'Thread-play':
-            self.audio_device_manager.play()
+            samples = self.generator.generate_sweep()
+
+            self.audio_device_manager.play(samples)
+
         else:
             self.audio_device_manager.record()
 
+        time.sleep(5)
+
         print("Exiting " + self.name)
+
